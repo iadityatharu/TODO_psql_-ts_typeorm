5477c5a7ec8fc5c76d8a22a618311ca348a1ed9d
LearnerAdityaa <adityatharudev@gmail.com>
2024-10-16 18:15:48 +0545

    fix: fixed bug on server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { AppDataSource } from "./source";
+import taskRoutes from "./routes/taskRoute";
 
 AppDataSource.initialize()
   .then(() => {
     console.log("Database is connected successfully");
     const app = express();
     app.use(express.json());
+    app.use("/api", taskRoutes);
     app.listen(process.env.PORT || 8080, () => {
       console.log(`Server is listening on ${process.env.PORT}`);
     });
